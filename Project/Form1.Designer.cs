4aa535704893c4422fa2441c5a0d1b9242ff63bf
Pavel <63100397+Gregg69@users.noreply.github.com>
2020-06-30 13:25:23 +0300

    Add files via upload

---
+++ Project/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace Equipment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.printDocument2 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Новый паспорт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 254);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(653, 114);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Все паспорта";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(118, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(189, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Модель";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(570, 387);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Печатать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(546, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(174, 23);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Записать в БД";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(386, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 22;
+            this.textBox2.Text = "01.05.2019";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(345, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "от";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(345, 50);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "label9";
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(15, 19);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(203, 20);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Паспорт устройства №";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(230, 19);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 20);
+            this.label11.TabIndex = 26;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(16, 53);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(26, 13);
+            this.label12.TabIndex = 27;
+            this.label12.Text = "Тип";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(118, 50);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(189, 21);
+            this.comboBox2.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Инвентарный №";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 30;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Серийный №";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(118, 130);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 32;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 13);
+            this.label7.TabIndex = 33;
+            this.label7.Text = "Профилактика";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(118, 161);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(42, 20);
+            this.textBox5.TabIndex = 34;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(16, 195);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(102, 13);
+            this.label13.TabIndex = 35;
+            this.label13.Text = "Срок амортизации";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(118, 192);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(42, 20);
+            this.textBox6.TabIndex = 36;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(345, 111);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(65, 13);
+            this.label14.TabIndex = 37;
+            this.label14.Text = "Поставщик";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(416, 104);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(192, 21);
+            this.comboBox3.TabIndex = 38;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(345, 147);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(33, 13);
+            this.label15.TabIndex = 39;
+            this.label15.Text = "Цена";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(418, 145);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 40;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(546, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 23);
+            this.button2.TabIndex = 41;
+            this.button2.Text = "Печатать паспорт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // printDocument2
+            // 
+            this.printDocument2.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument2_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 443);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Пасспорт устройства";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button2;
+        private System.Drawing.Printing.PrintDocument printDocument2;
+    }
+}
+
---
+++ Project/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Equipment
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection connection;
+        string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=equipment.mdb;";
+        OleDbDataAdapter adapter;
+        OleDbCommandBuilder commandBuilder;
+        DataSet ds;
+        int iRow, prodCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            connection = new OleDbConnection(connectionString);
+            connection.Open();
+            prodCount = 0;
+            iRow = 0;
+            //--------------
+            string query; OleDbCommand command; OleDbDataReader reader;
+
+            query = "SELECT * FROM [Типы оборудования]";
+            command = new OleDbCommand(query, connection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                // выводим данные столбцов текущей строки в listBox1
+                comboBox2.Items.Add(reader[0].ToString() + " " + reader[1].ToString() + " ");
+            }
+            comboBox2.SelectedIndex = 0;
+
+            query = "SELECT * FROM [Модели]";
+            command = new OleDbCommand(query, connection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                // выводим данные столбцов текущей строки в listBox1
+                comboBox1.Items.Add(reader[0].ToString() + " " + reader[1].ToString() + " ");
+            }
+            comboBox1.SelectedIndex = 0;
+
+            query = "SELECT * FROM [Поставщики]";
+            command = new OleDbCommand(query, connection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                // выводим данные столбцов текущей строки в listBox1
+                comboBox3.Items.Add(reader[0].ToString() + " " + reader[1].ToString() + " ");
+            }
+            comboBox3.SelectedIndex = 0;
+
+            //--------------
+            string sql = "SELECT * FROM [Оборудование]";
+            adapter = new OleDbDataAdapter(sql, connection);
+            ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.DataSource = ds.Tables[0];
+            label9.Text = DateTime.Now.ToString();
+
+
+        }
+
+        // Новый паспорт
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+            comboBox3.SelectedIndex = -1;
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "0";
+            textBox2.Text = "";
+            textBox7.Text = "";
+            textBox6.Text = "";
+            label11.Text = "?";
+        }
+
+        // Обновить поля на форме
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            iRow = dataGridView1.CurrentRow.Index;
+            label11.Text = dataGridView1.Rows[iRow].Cells[1].Value.ToString();
+            comboBox2.SelectedIndex = (int)dataGridView1.Rows[iRow].Cells[8].Value-1;
+            comboBox1.SelectedIndex = (int)dataGridView1.Rows[iRow].Cells[2].Value - 1;
+            comboBox3.SelectedIndex = (int)dataGridView1.Rows[iRow].Cells[7].Value - 1;
+            textBox3.Text = dataGridView1.Rows[iRow].Cells[1].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[iRow].Cells[3].Value.ToString();
+            textBox5.Text = "0"; //dataGridView1.Rows[iRow].Cells[3].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[iRow].Cells[4].Value.ToString();
+            textBox7.Text = dataGridView1.Rows[iRow].Cells[5].Value.ToString();
+            textBox6.Text = dataGridView1.Rows[iRow].Cells[6].Value.ToString();
+        }
+        //double kol = double.Parse(textBox1.Text);
+
+
+        // Закрыть форму
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            connection.Close();
+
+        }
+
+
+        // https://vscode.ru/prog-lessons/ms-access-i-c-sharp-rabotaem-s-bd.html
+        // Запись в БД
+        private void button4_Click(object sender, EventArgs e)
+        {
+        // Запись в таблицу Оборудование
+            string t1 = textBox3.Text;  // ИнвН
+            string t2 = (comboBox1.SelectedIndex + 1).ToString(); // ид модель
+            string t3 = textBox4.Text; // серийный
+            string t4 = textBox2.Text; // дата
+            string t5 = textBox7.Text;  // цена
+            string t6 = textBox6.Text;  // дата списания
+            string t7 = (comboBox3.SelectedIndex + 1).ToString(); ;  // ид поставщик
+            string t8 = (comboBox2.SelectedIndex + 1).ToString(); ;  // ид тип
+            string query = "INSERT INTO [Оборудование] ([ИнвN], [Id_модель], [СерийныйN], [Дата], [Цена], [Дата списания], [id_поставщик], [id_тип]) " +
+            "VALUES ("+
+            "'" + t1+"',"+ 
+            t2+", '"+
+            t3 + "','" +
+            t4 + "', " +
+            t5 + ",'" +
+            t6 + "'," +
+            t7 + "," +
+            t8 + 
+            ")";
+
+        // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+        OleDbCommand command = new OleDbCommand(query, connection);
+        // выполняем запрос к MS Access
+        command.ExecuteNonQuery();
+        MessageBox.Show("Паспорт успешно сохранен в БД");
+
+
+        // обновить таблицу на форме
+        //dataGridView1.Cl   
+        string sql = "SELECT * FROM [Оборудование]";
+        adapter = new OleDbDataAdapter(sql, connection);
+        ds = new DataSet();
+        adapter.Fill(ds);
+        dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        dataGridView1.AllowUserToAddRows = false;
+        dataGridView1.DataSource = ds.Tables[0];
+        label9.Text = DateTime.Now.ToString();
+
+            /*    
+
+            // Получить код
+            query = "SELECT [Код_заказ] FROM [Заказы]WHERE [Дата_создания]= "+
+                "'" + t4 + "'";
+            //query = "SELECT * FROM [Заказы]";// WHERE [Дата_создания]= "+
+            //command = new OleDbCommand(query, connection);
+            command.CommandText = query;
+            object o = command.ExecuteScalar();
+            string kod_zak = o.ToString();
+       
+                // Запись таблицы 
+            //object o;
+            string v1, v2, v3, v4;
+            for (int i = 0; i < dataGridView1.Rows.Count-1; ++i)
+            {
+                //o = dataGridView2[j, i].Value;
+                v1 = kod_zak;
+                v2 = dataGridView1[1, i].Value.ToString();
+                v3 = dataGridView1[4, i].Value.ToString();
+                v4 = dataGridView1[6, i].Value.ToString();
+
+                query = "INSERT INTO [Комплекты] ([Код_заказа], [Код_материала], [Количество], [Стоимость]) " +
+                       "VALUES (" +
+                       v1 + "," +
+                       v2 + "," +
+                       v3 + "," +
+                       v4 +
+                       ")";
+                command = new OleDbCommand(query, connection);
+                command.ExecuteNonQuery();
+                MessageBox.Show("Заказ успешно сохранен в БД");
+
+            };
+            */
+
+
+
+
+        }
+
+           // печать таблицы
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            printDocument1.Print();
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap bm = new Bitmap(this.dataGridView1.Width, this.dataGridView1.Height);
+            dataGridView1.DrawToBitmap(bm, new Rectangle(0, 0, this.dataGridView1.Width, this.dataGridView1.Height));
+            e.Graphics.DrawImage(bm, 0, 0);
+
+        }
+
+        // печать формы
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // https://docs.microsoft.com/ru-ru/dotnet/framework/winforms/advanced/how-to-print-a-windows-form?redirectedfrom=MSDN
+            dataGridView1.Visible = false;
+            CaptureScreen(); 
+            printDocument2.Print();
+            dataGridView1.Visible = true;
+        }
+
+
+        Bitmap memoryImage;
+
+        private void CaptureScreen()
+        {
+            Graphics myGraphics = this.CreateGraphics();
+            Size s = this.Size;
+            memoryImage = new Bitmap(s.Width, s.Height, myGraphics);
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+            memoryGraphics.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, s);
+        }
+
+        private void printDocument2_PrintPage(System.Object sender,
+               System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(memoryImage, 0, 0);
+        }
+
+
+    }
+}
